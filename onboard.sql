508a015c540b8e1efef7db7abad87729206b6911
= <theresamendoza1115@yahoo.com>
2015-03-24 12:12:54 -0700

    Updated database.

--- onboard.sql
+++ onboard.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `onboard` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `onboard`;
--- MySQL dump 10.13  Distrib 5.6.17, for Win32 (x86)
+-- MySQL dump 10.13  Distrib 5.6.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: onboard
 -- ------------------------------------------------------
--- Server version	5.6.19
+-- Server version	5.6.22-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -28,7 +26,8 @@ CREATE TABLE `inventory` (
   `item_id` int(11) NOT NULL,
   `item_name` varchar(45) NOT NULL,
   `item_qty` int(11) NOT NULL,
-  `item_desc` varchar(45) DEFAULT NULL,
+  `item_desc` varchar(150) DEFAULT NULL,
+  `item_details` mediumtext,
   `org_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`item_id`),
   KEY `idorg_idx` (`org_id`),
@@ -42,6 +41,7 @@ CREATE TABLE `inventory` (
 
 LOCK TABLES `inventory` WRITE;
 /*!40000 ALTER TABLE `inventory` DISABLE KEYS */;
+INSERT INTO `inventory` VALUES (1,'Animo Shirt',50,'Awesome green Animo Shirt!',NULL,2015002);
 /*!40000 ALTER TABLE `inventory` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -53,9 +53,8 @@ DROP TABLE IF EXISTS `manager`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `manager` (
-  `manager_id` int(4) NOT NULL,
-  `org_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`manager_id`),
+  `manager_id` int(11) NOT NULL,
+  `org_id` int(11) NOT NULL,
   KEY `org_id_idx` (`org_id`),
   CONSTRAINT `org_id` FOREIGN KEY (`org_id`) REFERENCES `organization` (`org_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -67,7 +66,7 @@ CREATE TABLE `manager` (
 
 LOCK TABLES `manager` WRITE;
 /*!40000 ALTER TABLE `manager` DISABLE KEYS */;
-INSERT INTO `manager` VALUES (1501,2015001);
+INSERT INTO `manager` VALUES (150001,2015001),(150001,2015002);
 /*!40000 ALTER TABLE `manager` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -79,15 +78,15 @@ DROP TABLE IF EXISTS `order`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `order` (
-  `order_id` int(11) NOT NULL,
+  `order_id` varchar(11) NOT NULL,
   `buyer_id` int(11) NOT NULL,
   `order_qty` int(11) NOT NULL,
   `item_id` int(11) NOT NULL,
   PRIMARY KEY (`order_id`),
   KEY `buyer_id_idx` (`buyer_id`),
   KEY `item_id_idx` (`item_id`),
-  CONSTRAINT `item_id` FOREIGN KEY (`item_id`) REFERENCES `inventory` (`item_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `buyer_id` FOREIGN KEY (`buyer_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `buyer_id` FOREIGN KEY (`buyer_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `item_id` FOREIGN KEY (`item_id`) REFERENCES `inventory` (`item_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -97,6 +96,7 @@ CREATE TABLE `order` (
 
 LOCK TABLES `order` WRITE;
 /*!40000 ALTER TABLE `order` DISABLE KEYS */;
+INSERT INTO `order` VALUES ('31415001A',11123456,1,1);
 /*!40000 ALTER TABLE `order` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -121,7 +121,7 @@ CREATE TABLE `organization` (
 
 LOCK TABLES `organization` WRITE;
 /*!40000 ALTER TABLE `organization` DISABLE KEYS */;
-INSERT INTO `organization` VALUES (2015001,'La Salle Computer Society','p@ss');
+INSERT INTO `organization` VALUES (2015001,'La Salle Computer Society','p@ss'),(2015002,'Computer Studies Government','p@ss');
 /*!40000 ALTER TABLE `organization` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -141,8 +141,7 @@ CREATE TABLE `user` (
   `manager_id` int(4) DEFAULT NULL,
   `password` varchar(45) NOT NULL,
   PRIMARY KEY (`user_id`),
-  KEY `manager_id_idx` (`manager_id`),
-  CONSTRAINT `manager_id` FOREIGN KEY (`manager_id`) REFERENCES `manager` (`manager_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `manager_id_idx` (`manager_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -152,7 +151,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (11123456,'Juan','Dela Cruz','juandelacruz@gmail.com','Student',NULL,''),(11234567,'Derpina','Chualongkorn','derpinaxxx@yahoo.com','Student',1501,'');
+INSERT INTO `user` VALUES (11123456,'Juan','Dela Cruz','juandelacruz@gmail.com','Student',NULL,'secret'),(11234567,'Derpina','Chua','derpinaxxx@yahoo.com','Student',150001,'secret');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -165,4 +164,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-03-18 21:52:41
+-- Dump completed on 2015-03-24 12:20:34
